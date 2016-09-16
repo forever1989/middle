044daec9a983213161e6af2256188d29ee8825d0
cuiheng01 <cuiheng@wondersgroup.com>
2016-09-16 23:40:54 +0800

    测试

---
+++ src/com/java/main/controller/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.java.main.controller;
+
+/**
+ * @author admin
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
